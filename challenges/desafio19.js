8f2fe06e44b704c89bd7b61ca225396c536a268e
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-03-20 10:30:03 -0300

    feat: desafio19

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,5 +1,5 @@
-// // Crie uma query que faça a remoção do item cebola em todos os sanduíches.
-// db.produtos.updateMany({}, { $pull: { ingredientes: "cebola" } });
+// Crie uma query que faça a remoção do item cebola em todos os sanduíches.
+db.produtos.updateMany({}, { $pull: { ingredientes: "cebola" } });
 
-// // Crie uma query que retorne o nome e ingredientes de todos os documentos.
-// db.produtos.find({}, { name: 1, ingredientes: 1, _id: 0 });
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
